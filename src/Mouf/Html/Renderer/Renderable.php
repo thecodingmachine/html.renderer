3e7ad0dd482f1df15e2a4801d85e653c495aac4b
David Négrier <d.negrier@thecodingmachine.com>
2015-02-09 15:20:37 +0100

    Migrating to PSR-2

--- src/Mouf/Html/Renderer/Renderable.php
+++ src/Mouf/Html/Renderer/Renderable.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Mouf\Html\Renderer;
 
@@ -8,34 +8,38 @@ use Mouf\RootContainer;
 /**
  * Classes using this trait will have an automatic implementation of the toHtml method provided that
  * will call the default rendering system.
- * 
+ *
  * @author David Négrier <david@mouf-php.com>
  */
-trait Renderable {
+trait Renderable
+{
 
-	/**
-	 * @var string
-	 */
-	protected $context = null;
-	
-	/**
-	 * Returns an array containing all the public and protected properties.
-	 * 
-	 * @return array
-	 */
-	public function getPrivateProperties() {
-		return get_object_vars($this);
-	}
-	
-	/**
-	 * Set the context, this is a string that is appended the template file name
-	 * @param string $context
-	 */
-	public function setContext($context) {
-		$this->context = $context;
-	}
-	
-	public function toHtml() {
-		RootContainer::get('defaultRenderer')->render($this, $this->context);
-	}
+    /**
+     * @var string
+     */
+    protected $context = null;
+
+    /**
+     * Returns an array containing all the public and protected properties.
+     *
+     * @return array
+     */
+    public function getPrivateProperties()
+    {
+        return get_object_vars($this);
+    }
+
+    /**
+     * Set the context, this is a string that is appended the template file name
+     * @param string $context
+     */
+    public function setContext($context)
+    {
+        $this->context = $context;
+    }
+
+    public function toHtml()
+    {
+        RootContainer::get('defaultRenderer')->render($this, $this->context);
+    }
 }
